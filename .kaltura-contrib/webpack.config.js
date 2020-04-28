cb5870ede019c25354545311de37a4534bf01f4f
Serhii Marchenko <serhii.marchenko@Kaltura.com>
2020-04-28 11:30:09 +0300

    chore(ui): upgrade webpack (FEV-557)

--- .kaltura-contrib/webpack.config.js
+++ .kaltura-contrib/webpack.config.js
@@ -1,5 +1,7 @@
 module.exports = (config, mode) => {
-    // mode = production | dev (following the dev flag provided)
-    return config;
+  // mode = production | dev (following the dev flag provided)
+  return {
+    ...config,
+    externals: {...config.externals, preact: 'root KalturaPlayer.ui.preact'},
+  };
 };
-
