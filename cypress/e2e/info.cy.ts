2e1cb6dc95c3fd1af112efbceb89ba5b5e16e4ff
Serhii <serhii.marchenko@Kaltura.com>
2023-01-09 15:52:05 +0200

    fix(FEV-604): no rows separation when using the Enter key in the description (#41)
    * fix(FEV-604): multiline support
    
    * fix(FEV-604): upd dependencies

--- cypress/e2e/info.cy.ts
+++ cypress/e2e/info.cy.ts
@@ -58,4 +58,15 @@ describe('Info plugin', () => {
       expect($div.text()).to.eq('Live Now');
     });
   });
+
+  it('should sanitize and render discription with html tags inside', () => {
+    const result = `line one<br>line two<br>line three<br>Praesent sapien massa, convallis a <u>pellentesque nec, egest</u>as non nisi. Mauris blandit aliquet elit, eget <a target="_blank" href="http://google.com">tincidunt</a> nibh pulvinar a. <b>Curabitur arcu erat, accumsan </b>id imperdiet et, <span>porttitor</span> at sem.<i> Vestibulum ante ipsum prim</i>is in faucibus;<br><blockquote><ul><li>one</li><li>two</li></ul></blockquote>Last line<br><br>`;
+    cy.intercept('POST', 'http://mock-api/service/multirequest', {fixture: 'vod-with-html-tags.json'});
+    cy.visit('index.html');
+    cy.get('.playkit-pre-playback-play-button').click({force: true});
+    cy.get('[data-testid="infoPluginButton"]').click({force: true});
+    cy.get('[data-testid="entryDescription"]').should($div => {
+      expect($div.html()).to.eq(result);
+    });
+  });
 });
