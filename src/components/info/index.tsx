0900008da6003e3a0fa5571ae944ebe9aba05706
Serhii Marchenko <serhii.marchenko@kaltura.com>
2023-10-30 18:58:46 +0200

    feat(FEC-13143): add media info

--- src/components/info/index.tsx
+++ src/components/info/index.tsx
@@ -1,4 +1,4 @@
-import {h, Component} from 'preact';
+import {h, Component, VNode} from 'preact';
 import * as styles from './info.scss';
 import * as sanitizeHtml from 'sanitize-html';
 import {OverlayPortal} from '@playkit-js/common/dist/hoc/overlay-portal';
@@ -7,12 +7,15 @@ const {
   redux: {connect}
 } = KalturaPlayer.ui;
 const {Overlay} = KalturaPlayer.ui.components;
+const {Text} = KalturaPlayer.ui.preacti18n;
 
 export interface InfoProps {
   onClick: () => void;
   entryName: string;
   description: string;
-  broadcastedDate: string;
+  creator: string;
+  updatedAt: VNode;
+  views: string;
 }
 interface ConnectProps {
   playerSize?: string;
@@ -25,8 +28,33 @@ const mapStateToProps = (state: Record<string, any>) => ({
 });
 @connect(mapStateToProps)
 export class Info extends Component<MergedProps> {
+  renderMediaInfo = () => {
+    const {creator, updatedAt, views} = this.props;
+    const mediaInfo = [creator, views, updatedAt];
+    if (!mediaInfo.every(v => v)) {
+      return null;
+    }
+    const dataTestIds = ['creator', 'views', 'updatedAt'];
+    return (
+      <div className={styles.mediaInfo} data-testid="mediaInfo">
+        {mediaInfo.map((val, index) => {
+          if (!val) {
+            return null;
+          }
+          return (
+            <div key={index} className={styles.mediaInfoBlock} data-testid={dataTestIds[index]}>
+              {index === 0 ? <Text id="info.creator">By </Text> : null}
+              {val}
+              {index === 1 ? <Text id="info.views"> views</Text> : null}
+            </div>
+          );
+        })}
+      </div>
+    );
+  };
+
   render(props: MergedProps) {
-    const {onClick, entryName, description, broadcastedDate, playerSize = ''} = props;
+    const {onClick, entryName, description, playerSize = ''} = props;
     if (playerSize === PLAYER_SIZE.TINY) {
       return null;
     }
@@ -34,14 +62,10 @@ export class Info extends Component<MergedProps> {
       <OverlayPortal>
         <Overlay open onClose={onClick}>
           <div className={[styles.infoRoot, styles[playerSize]].join(' ')} data-testid="infoRoot">
-            {broadcastedDate && (
-              <div className={styles.broadcastDate} data-testid="broadcastedDate">
-                {broadcastedDate}
-              </div>
-            )}
             <div className={styles.entryName} data-testid="entryName">
               {entryName}
             </div>
+            {this.renderMediaInfo()}
             {description && (
               <div data-testid="entryDescription" className={styles.entryDescription} dangerouslySetInnerHTML={{__html: sanitizeHtml(description)}} />
             )}
