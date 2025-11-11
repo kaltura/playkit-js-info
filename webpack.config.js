6a55111e6f5fd9e4ec9063c7e091753a9ee3d366
SivanA-Kaltura <88330203+SivanA-Kaltura@users.noreply.github.com>
2025-11-11 14:44:03 +0200

    add demo page

--- webpack.config.js
+++ webpack.config.js
@@ -64,8 +64,9 @@ module.exports = {
     ]
   },
   devServer: {
-    static: __dirname + '/src'
-    // host: '192.168.68.107'
+    static: {
+      directory: path.join(__dirname, 'demo')
+    }
   },
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
