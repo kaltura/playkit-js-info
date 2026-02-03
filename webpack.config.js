19015c50fabfb494161236aeba64646de4a41470
Sivan Agranov <88330203+SivanA-Kaltura@users.noreply.github.com>
2026-02-03 10:41:18 +0200

    fix(FEC-14873): Duplicate <style> tags appear on the page (#125)
    * Update webpack.config.js
    
    * Update run_canary.yaml
    
    * Update webpack.config.js
    
    * Update webpack.config.js
    
    * Update run_canary.yaml

--- webpack.config.js
+++ webpack.config.js
@@ -41,6 +41,7 @@ module.exports = {
           {
             loader: 'style-loader',
             options: {
+              injectType: "singletonStyleTag",
               attributes: {
                 id: `${packageData.name}`
               },
@@ -74,6 +75,7 @@ module.exports = {
   },
   externals: {
     '@playkit-js/kaltura-player-js': 'root KalturaPlayer',
+    '@playkit-js/playkit-js': 'root KalturaPlayer.core',
     preact: 'root KalturaPlayer.ui.preact'
   }
 };
